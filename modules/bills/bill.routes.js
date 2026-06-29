b3bec8f777df276fa08d34d286f5ac365c180f29
Himanshu <mauryahimanshu416@gmail.com>
2026-06-29 14:33:10 +0530

    everything works fine

--- modules/bills/bill.routes.js
+++ modules/bills/bill.routes.js
@@ -1,20 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const billController = require('./bill.controller');
-const { auth } = require('../../middleware/auth.middleware');
-const { authorize } = require('../../middleware/role.middleware');
-const { ROLES } = require('../../constants/roles');
+const billController = require("./bill.controller");
+const { auth } = require("../../middleware/auth.middleware");
+const { authorize } = require("../../middleware/role.middleware");
+const { ROLES } = require("../../constants/roles");
 
 router.use(auth);
 
-router.post('/', authorize(ROLES.SALES), billController.createBill);
-router.get('/', billController.getBills);
-router.get('/:id', billController.getBillById);
-router.put('/:id', authorize(ROLES.SALES, ROLES.SUPERADMIN), billController.updateBill);
-router.delete('/:id', authorize(ROLES.SALES, ROLES.SUPERADMIN), billController.deleteBill);
-router.post('/:id/submit', authorize(ROLES.SALES), billController.submitBill);
-router.post('/:id/approve', authorize(ROLES.ACCOUNTANT), billController.approveBill);
-router.post('/:id/reject', authorize(ROLES.ACCOUNTANT), billController.rejectBill);
-router.post('/:id/send-email', authorize(ROLES.SALES), billController.sendBillEmailToClient);
+router.post("/", authorize(ROLES.SALES), billController.createBill);
+router.get("/", billController.getBills);
+router.get("/:id", billController.getBillById);
+router.put("/:id", billController.updateBill);
+router.delete(
+  "/:id",
+  authorize(ROLES.SALES, ROLES.SUPERADMIN),
+  billController.deleteBill,
+);
+router.post("/:id/submit", authorize(ROLES.SALES), billController.submitBill);
+router.post(
+  "/:id/approve",
+  authorize(ROLES.ACCOUNTANT),
+  billController.approveBill,
+);
+router.post(
+  "/:id/reject",
+  authorize(ROLES.ACCOUNTANT),
+  billController.rejectBill,
+);
+router.post(
+  "/:id/send-email",
+  authorize(ROLES.SALES),
+  billController.sendBillEmailToClient,
+);
 
 module.exports = router;
