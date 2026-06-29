6a3371c06b8fb89513f6f74f895fb5f8db13dd4e
Himanshu <mauryahimanshu416@gmail.com>
2026-06-29 17:49:37 +0530

    working

--- modules/clients/client.model.js
+++ modules/clients/client.model.js
@@ -1,43 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const clientSchema = new mongoose.Schema({
-  representativeName: {
-    type: String,
-    required: true,
+const clientSchema = new mongoose.Schema(
+  {
+    representativeName: {
+      type: String,
+      required: true,
+    },
+    companyName: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+    gstNumber: {
+      type: String,
+    },
+    salesRepName: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
   },
-  companyName: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  phone: {
-    type: String,
-    required: true,
-  },
-  address: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-  },
-  gstNumber: {
-    type: String,
-  },
-  salesRepName: {
-    type: String,
-  },
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-}, {
-  timestamps: true,
-});
+);
 
-module.exports = mongoose.model('Client', clientSchema);
+module.exports = mongoose.model("Client", clientSchema);
